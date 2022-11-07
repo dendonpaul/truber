7d752701efeaa7385a2f971bb1709e31fed8fdf7
dendonpaul <dendonpaul@gmail.com>
2022-11-07 23:42:50 +0530

    owner model created

---
+++ server/models/OwnerModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+
+const OwnerSchema = new mongoose.Schema({
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  address: { type: String, required: true },
+  userType: {
+    type: String,
+    required: true,
+    default: "owner",
+  },
+  verification: {
+    type: String,
+  },
+  verified: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+});
+
+const OwnerModel = mongoose.model("owners", OwnerSchema);
+
+module.exports = OwnerModel;
